eb5aed159c1c85db7a2f281798be2194bc925a61
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-11-28 20:03:50 -0800

    commit tree c544420af8633be6e19c81a49c62599bb4853258

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items, only: [:index] do 
+    resources :users, only: [:show]
+  end 
+  resources :users, only: [:show] do 
+    resources :items, only: [:index, :show, :create]
+  end 
 end
